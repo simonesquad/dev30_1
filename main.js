0d151f57c5481b42d165749a8f135d00045a4e04
Simone Ballard <simone.a.ballard@gmail.com>
2022-04-25 16:06:29 -0500

    submission

---
+++ main.js
@@ -0,0 +1,28 @@
+// Step 1: creating a variable of my individal age as a number type
+var myAge = 20
+
+// Step 2: creating a variable for early years that is subject to change
+var earlyYears = 2
+
+// Step 3:
+earlyYears *= 10.5
+
+// Step 4: altering the myAge variable 
+var laterYears = myAge - 2
+
+// Step 5:
+var dogYears = laterYears * 4
+
+// Step 6: Sanity Check
+console.log(earlyYears)
+console.log(laterYears)
+
+// Step 7: the combination step
+var myAgeInDogYears = earlyYears + laterYears
+
+// Step 8: simply naming myself
+string = 'Simone'
+var myName = string.toLowerCase()
+
+// Step 9: use of temperate literals
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
